c0de8e3de71392e73f24b90529e090d95e21c24a
Alyssa Kwong-Taylor <a.kwong-taylor@outlook.com>
2019-10-15 16:52:06 -0700

    Set up Table Making functions in JS for Tablet and Desktop site ver. Class naming implementations to be added later.

--- sprint-2/scripts/showsTable.js
+++ sprint-2/scripts/showsTable.js
@@ -7,21 +7,26 @@ var showTimes = [
     {Dates: "Fri Sep 05 2019", Venue: "Moscow Center", Location: "San Francisco, CA"} 
 ]
 
-function tableMaker (targetTable, roster) {
-    for (var student of roster) {
+function makeTable (targetTable, data) {
+    for (var show of data) {
         var row = targetTable.insertRow(); 
-        for (var key in student) {
+        for (var key in show) {
             var cell = row.insertCell(); 
-            var text = document.createTextNode(student[key]); // Creates #text Node
+            var text = document.createTextNode(show[key]); //Creates #text Node
             cell.appendChild(text); 
-        }
+        } 
+        var cell = row.insertCell(); 
+        var btn = document.createElement("button"); 
+        btn.textContent = "Buy Tickets"; 
+        cell.appendChild(btn); 
+
     }
 }
 
-function tableHeadMaker (targetTable, rosterKeys) {
+function makeTableHead (targetTable, dataKeys) {
     var tHead = table.createTHead(); // Creates TH Node 
     var row = tHead.insertRow(); 
-    for (var key of rosterKeys) {
+    for (var key of dataKeys) {
         var th = document.createElement("th"); 
         var text = document.createTextNode(key); 
         th.appendChild(text); 
@@ -30,7 +35,6 @@ function tableHeadMaker (targetTable, rosterKeys) {
 }
 
 var table = document.querySelector("table"); 
-console.log(table); 
-var rosterKeys = Object.keys(roster[0]); 
-tableMaker(table, roster); 
-tableHeadMaker (table, rosterKeys); 
+var showTimeKeys = Object.keys(showTimes[0]); 
+makeTable(table, showTimes); 
+makeTableHead (table, showTimeKeys); 
