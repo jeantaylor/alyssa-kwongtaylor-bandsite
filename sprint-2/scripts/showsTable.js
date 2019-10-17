058509ad4e3483922fe50deae17ec19e78e87746
Alyssa Kwong-Taylor <a.kwong-taylor@outlook.com>
2019-10-17 11:38:33 -0700

    Noor helped me fix my mobile table function~ THANKS NOOR :D

--- sprint-2/scripts/showsTable.js
+++ sprint-2/scripts/showsTable.js
@@ -1,12 +1,3 @@
-let showTimes = [
-    {Dates: "Mon Dec 17 2018", Venue: "Ronald Lane", Location: "San Francisco, CA"},
-    {Dates: "Tue Jul 18 2019", Venue: "Pier 3 East", Location: "San Francisco, CA"},
-    {Dates: "Fri Jul 22 2019", Venue: "View Lounge", Location: "San Francisco, CA"},
-    {Dates: "W Aug 11 2019", Venue: "Pres Club", Location: "San Francisco, CA"},
-    {Dates: "Sa Aug 12 2019", Venue: "Hyatt Agency", Location: "San Francisco, CA"},
-    {Dates: "Fri Sep 05 2019", Venue: "Moscow Center", Location: "San Francisco, CA"} 
-]
-
 function makeTable (targetTable, schedule) {
     for (let show of schedule) { // Make 6
         let row = targetTable.insertRow(); // Inserts <tr> into HTML
@@ -23,10 +14,11 @@ function makeTable (targetTable, schedule) {
     }
 }
 
-function makeTableHead (targetTable, dataKeys) {
+function makeTableHead (targetTable, schedule) {
+    let scheduleKeys = Object.keys(schedule[0]); 
     let tHead = table.createTHead(); // Creates TH Node 
     let row = tHead.insertRow(); 
-    for (let key of dataKeys) {
+    for (let key of scheduleKeys) {
         let th = document.createElement("th"); 
         let text = document.createTextNode(key); 
         th.appendChild(text); 
@@ -35,17 +27,23 @@ function makeTableHead (targetTable, dataKeys) {
 }
 
 function makeTableMobile (targetTable, schedule) {
+    let scheduleKeys = Object.keys(schedule[0]); 
     let row = targetTable.insertRow(); // Inserts 1 row
     for (show of schedule) {
         let cell = row.insertCell(); 
-        for (i = 0; i < schedule.length / 2; i++) {
+        for (i = 0; i < 3; i++) {
+            console.log(show, i)
             let header = document.createElement('div'); 
-            let text = document.createTextNode(Object.keys(schedule[i])[i]); 
+            let text = document.createTextNode(scheduleKeys[i]); 
+            console.log(header); 
+            console.log(text); 
             header.appendChild(text);
             cell.appendChild(header);  
 
             let content = document.createElement('div'); 
-            text = document.createTextNode(Object.values(schedule[i])[i]); 
+            text = document.createTextNode(Object.values(show)[i]); 
+            console.log(content); 
+            console.log(text); 
             content.appendChild(text); 
             cell.appendChild(content); 
         }
@@ -55,8 +53,32 @@ function makeTableMobile (targetTable, schedule) {
     }
 }
 
-let tabDeskMediaQuery = window.matchMedia("(min-width: 768px)")
+let showTimes = [
+    {Dates: "Mon Dec 17 2018", Venue: "Ronald Lane", Location: "San Francisco, CA"},
+    {Dates: "Tue Jul 18 2019", Venue: "Pier 3 East", Location: "San Francisco, CA"},
+    {Dates: "Fri Jul 22 2019", Venue: "View Lounge", Location: "San Francisco, CA"},
+    {Dates: "W Aug 11 2019", Venue: "Pres Club", Location: "San Francisco, CA"},
+    {Dates: "Sa Aug 12 2019", Venue: "Hyatt Agency", Location: "San Francisco, CA"},
+    {Dates: "Fri Sep 05 2019", Venue: "Moscow Center", Location: "San Francisco, CA"} 
+]
+
 let table = document.querySelector("table"); 
-let showTimeKeys = Object.keys(showTimes[0]); 
+let tableMediaQuery = window.innerWidth; 
+
+if (tableMediaQuery < 768) {
+    makeTable(table, showTimes); 
+    makeTableHead (table, showTimes); 
+} else {
+    makeTableMobile(table, showTimes); 
+}
+
+/**
 makeTable(table, showTimes); 
-makeTableHead (table, showTimeKeys); 
+makeTableHead (table, showTimes); 
+
+if (tableMediaQuery.matches) {
+    console.log("The screen is AT LEAST 768px wide.");
+} else {
+    console.log("The screen is less than 768px wide.");
+}
+*/
