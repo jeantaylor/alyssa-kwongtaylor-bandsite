8f95f056618742b8707b28676788c18c76dcfe8d
Alyssa Kwong-Taylor <a.kwong-taylor@outlook.com>
2019-10-17 17:25:17 -0700

    Shows page and table finished and styled. Functionality to run certain table making fn on window resize event to be added.

--- sprint-2/scripts/showsTable.js
+++ sprint-2/scripts/showsTable.js
@@ -1,14 +1,18 @@
 function makeTable (targetTable, schedule) {
+    targetTable.classList.add("shows__table"); 
     for (let show of schedule) { // Make 6
         let row = targetTable.insertRow(); // Inserts <tr> into HTML
         for (let key in show) { // Make 3
             let cell = row.insertCell(); // Inserts <td> in <tr> 
             let text = document.createTextNode(show[key]); //Creates #text Node
             cell.appendChild(text); 
+            cell.classList.add("shows__table-content"); 
         } 
         let cell = row.insertCell(); // Inserts 1 additional <td> per row for btn to sit
+        cell.classList.add("shows__table-content");
         let btn = document.createElement("button"); 
         btn.textContent = "Buy Tickets"; 
+        btn.classList.add("shows__cta"); 
         cell.appendChild(btn); 
 
     }
@@ -17,6 +21,7 @@ function makeTable (targetTable, schedule) {
 function makeTableHead (targetTable, schedule) {
     let scheduleKeys = Object.keys(schedule[0]); 
     let tHead = table.createTHead(); // Creates TH Node 
+    tHead.classList.add("shows__table-header"); 
     let row = tHead.insertRow(); 
     for (let key of scheduleKeys) {
         let th = document.createElement("th"); 
@@ -27,10 +32,13 @@ function makeTableHead (targetTable, schedule) {
 }
 
 function makeTableMobile (targetTable, schedule) {
+    targetTable.classList.add("shows__table"); 
     let scheduleKeys = Object.keys(schedule[0]); 
     let row = targetTable.insertRow(); // Inserts 1 row
+    row.classList.add("shows__table--mobile"); 
     for (show of schedule) {
         let cell = row.insertCell(); 
+        cell.classList.add("shows__table-show"); 
         for (i = 0; i < 3; i++) {
             console.log(show, i)
             let header = document.createElement('div'); 
@@ -39,6 +47,7 @@ function makeTableMobile (targetTable, schedule) {
             console.log(text); 
             header.appendChild(text);
             cell.appendChild(header);  
+            header.classList.add("shows__table-header"); 
 
             let content = document.createElement('div'); 
             text = document.createTextNode(Object.values(show)[i]); 
@@ -46,9 +55,11 @@ function makeTableMobile (targetTable, schedule) {
             console.log(text); 
             content.appendChild(text); 
             cell.appendChild(content); 
+            content.classList.add("shows__table-content"); 
         }
-        let btn = document.createElement("button"); 
+        let btn = document.createElement("button");
         btn.textContent = "Buy Tickets"; 
+        btn.classList.add("shows__cta");
         cell.appendChild(btn);
     }
 }
@@ -57,7 +68,7 @@ let showTimes = [
     {Dates: "Mon Dec 17 2018", Venue: "Ronald Lane", Location: "San Francisco, CA"},
     {Dates: "Tue Jul 18 2019", Venue: "Pier 3 East", Location: "San Francisco, CA"},
     {Dates: "Fri Jul 22 2019", Venue: "View Lounge", Location: "San Francisco, CA"},
-    {Dates: "W Aug 11 2019", Venue: "Pres Club", Location: "San Francisco, CA"},
+    {Dates: "Wed Aug 11 2019", Venue: "Pres Club", Location: "San Francisco, CA"},
     {Dates: "Sa Aug 12 2019", Venue: "Hyatt Agency", Location: "San Francisco, CA"},
     {Dates: "Fri Sep 05 2019", Venue: "Moscow Center", Location: "San Francisco, CA"} 
 ]
@@ -71,3 +82,4 @@ if (tableMediaQuery.matches) {
 } else {
     makeTableMobile(table, showTimes); 
 }
+
